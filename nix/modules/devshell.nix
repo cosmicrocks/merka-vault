000cdfaa773b85abb2515ecaa9ba64151f949c4d
Itamar Perez <itamarperez@gmail.com>
2025-02-17 12:17:12 -0800

    Add OpenSSL to development shell build inputs

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,19 +1,29 @@
 { inputs, ... }:
 {
-  perSystem = { config, self', pkgs, lib, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "merka-vault-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-      ];
-      packages = with pkgs; [
-        just
-        nixd # Nix language server
-        bacon
-        act
-        config.process-compose.cargo-doc-live.outputs.package
-      ];
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "merka-vault-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        buildInputs = with pkgs; [
+          openssl
+        ];
+        packages = with pkgs; [
+          just
+          nixd # Nix language server
+          bacon
+          act
+          config.process-compose.cargo-doc-live.outputs.package
+        ];
+      };
     };
-  };
 }
